5e7d5b53eeb2af7096e7bb1bffdcf3a96e028b51
juliobra <118060917+juliobra@users.noreply.github.com>
2022-11-29 16:02:44 -0300

    cors

--- src/main/java/com/example/demo/controller/Juegocontroller.java
+++ src/main/java/com/example/demo/controller/Juegocontroller.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/api")
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200")
 public class Juegocontroller {
 
     @Autowired
